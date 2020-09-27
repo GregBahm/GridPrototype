a6ca7cff887b01e7cabf029d7c7f04821a2bd5e5
Owner <Owner@DESKTOP-201N9BU>
2020-09-27 09:58:59 -0700

    Starting city mesh builder

---
+++ GridUnityProject/Assets/CityBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GridMaking;
+using System;
+
+[RequireComponent(typeof(GridMaker))]
+class CityBuilder : MonoBehaviour
+{
+    private MeshCollider meshCollider;
+    private Mesh mesh;
+
+    private void Start()
+    {
+        mesh = GetMesh();
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = mesh;
+    }
+
+    private Mesh GetMesh()
+    {
+        Mesh mesh = new Mesh();
+        return mesh;
+    }
+}
