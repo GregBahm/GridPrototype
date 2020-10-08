f54f4a44c815c0a9549d8705464fe2db6d990af7
Owner <Owner@DESKTOP-201N9BU>
2020-10-08 04:25:05 -0700

    Working on getting the logic with getting the logic on the lids right.

--- GridUnityProject/Assets/NewMeshMaker.cs
+++ GridUnityProject/Assets/NewMeshMaker.cs
@@ -48,7 +48,7 @@ public class InteractionMesh
 
     private IEnumerable<IMeshContributor> GetMeshContributors(MainGrid grid)
     {
-        IMeshContributor[] groundContributor = grid.Points.Where(item => !item.Voxels[0].Filled).Select(item => new GroundMeshContributor(item)).ToArray();
+        IMeshContributor[] groundContributor = grid.Points.Where(item => !item.Voxels[0].Filled).Select(item => new HorizontalMeshContributor(item.Voxels[0], null)).ToArray();
         IMeshContributor[] contributors = grid.FilledCells.Select(item => new CellMeshContributor(item)).ToArray();
         return groundContributor.Concat(contributors);
     }
@@ -62,25 +62,33 @@ public class InteractionMesh
         public VoxelCell TargetCell { get; }
         public VoxelCell SourceCell { get; }
 
-        public MeshBuilderTriangle(VoxelCell targetCell, VoxelCell sourceCell, IMeshBuilderPoint pointA, IMeshBuilderPoint pointB, IMeshBuilderPoint pointC)
+        public MeshBuilderTriangle(VoxelCell targetCell, 
+            VoxelCell sourceCell, 
+            IMeshBuilderPoint pointA, 
+            IMeshBuilderPoint pointB, 
+            IMeshBuilderPoint pointC)
         {
             TargetCell = targetCell;
             SourceCell = sourceCell;
             PointA = pointA;
-            bool reorderVerts = GetShouldReorderVerts(sourceCell, pointA.Position, pointB.Position, pointC.Position);
+            bool reorderVerts = sourceCell == null?
+                GetShouldReorderVerts(pointA.Position, pointB.Position, pointC.Position) : // Is a basepoint
+                GetShouldReorderVerts(targetCell, pointA.Position, pointB.Position, pointC.Position);
             PointB = reorderVerts ? pointB : pointC;
             PointC = reorderVerts ? pointC : pointB;
         }
 
-        private bool GetShouldReorderVerts(VoxelCell sourceCell, Vector3 pointA, Vector3 pointB, Vector3 pointC)
+        private bool GetShouldReorderVerts(Vector3 pointA, Vector3 pointB, Vector3 pointC)
         {
             Vector3 crossVector = Vector3.Cross(pointA - pointB, pointA - pointC);
-            if(sourceCell == null) // it's a ground cell. Just make sure the cross product points upward
-            {
-                return crossVector.y > 0;
-            }
-            Vector3 toSource = pointA - sourceCell.CellPosition;
-            return Vector3.Dot(toSource, crossVector) > 0; // TODO: Test this
+            return crossVector.y > 0;
+        }
+
+        private bool GetShouldReorderVerts(VoxelCell targetCell, Vector3 pointA, Vector3 pointB, Vector3 pointC)
+        {
+            Vector3 crossVector = Vector3.Cross(pointA - pointB, pointA - pointC);
+            Vector3 toTarget = pointA - targetCell.CellPosition;
+            return Vector3.Dot(toTarget, crossVector) < 0;
         }
     }
 
@@ -119,32 +127,31 @@ public class InteractionMesh
         }
     }
 
-    private class GroundMeshContributor : IMeshContributor
+    private class HorizontalMeshContributor : IMeshContributor
     {
         public IEnumerable<IMeshBuilderPoint> Points { get; }
 
         public IEnumerable<MeshBuilderTriangle> Triangles { get; }
 
-        public GroundMeshContributor(GroundPoint groundPoint)
+        public HorizontalMeshContributor(VoxelCell baseCell, VoxelCell sourceCell)
         {
             List<IMeshBuilderPoint> points = new List<IMeshBuilderPoint>();
             List<MeshBuilderTriangle> triangles = new List<MeshBuilderTriangle>();
 
-            VoxelCell baseCell = groundPoint.Voxels[0];
             IMeshBuilderPoint corePoint = new MeshBuilderCorePoint(baseCell);
 
             points.Add(corePoint);
-            foreach (GroundQuad quad in groundPoint.PolyConnections)
+            foreach (GroundQuad quad in baseCell.GroundPoint.PolyConnections)
             {
-                GroundPoint diagonal = quad.GetDiagonalPoint(groundPoint);
-                Vector3 quadPos = new Vector3(quad.Center.x, 0, quad.Center.y);
+                GroundPoint diagonal = quad.GetDiagonalPoint(baseCell.GroundPoint);
+                Vector3 quadPos = new Vector3(quad.Center.x, baseCell.Height, quad.Center.y);
                 IMeshBuilderPoint diagonalPoint = new MeshBuilderSubPoint(baseCell, diagonal.Voxels[0], quadPos);
-                GroundPoint[] otherPoints = quad.Points.Where(item => item != groundPoint && item != diagonal).ToArray();
+                GroundPoint[] otherPoints = quad.Points.Where(item => item != baseCell.GroundPoint && item != diagonal).ToArray();
                 IMeshBuilderPoint otherPointA = new MeshBuilderSubPoint(baseCell, otherPoints[0].Voxels[0]);
                 IMeshBuilderPoint otherPointB = new MeshBuilderSubPoint(baseCell, otherPoints[1].Voxels[0]);
 
-                MeshBuilderTriangle triangleA = new MeshBuilderTriangle(baseCell, null, corePoint, diagonalPoint, otherPointA);
-                MeshBuilderTriangle triangleB = new MeshBuilderTriangle(baseCell, null, diagonalPoint, corePoint, otherPointB);
+                MeshBuilderTriangle triangleA = new MeshBuilderTriangle(baseCell, sourceCell, corePoint, diagonalPoint, otherPointA);
+                MeshBuilderTriangle triangleB = new MeshBuilderTriangle(baseCell, sourceCell, diagonalPoint, corePoint, otherPointB);
 
                 points.Add(diagonalPoint);
                 points.Add(otherPointA);
@@ -171,11 +178,13 @@ public class InteractionMesh
             List<IMeshContributor> subContributors = new List<IMeshContributor>();
             if(GetDoesHaveBottom())
             {
-                // TODO: Add bottom cap to subContributors
+                HorizontalMeshContributor groundContributor = new HorizontalMeshContributor(cell, cell.CellBelow);
+                subContributors.Add(groundContributor);
             }
             if (GetDoesHaveTop())
             {
-                // TODO: Add top cap to subContributors
+                HorizontalMeshContributor groundContributor = new HorizontalMeshContributor(cell, cell.CellAbove);
+                subContributors.Add(groundContributor);
             }
             SideToFill[] sidesToFill = GetSidesToFill().ToArray();
             subContributors.AddRange(sidesToFill);
