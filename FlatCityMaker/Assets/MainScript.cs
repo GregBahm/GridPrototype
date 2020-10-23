80ce1dfe440f744670418005029f2e71e0d8fc24
Greg Bahm <grbahm@microsoft.com>
2020-10-23 13:30:11 -0700

    Doesn't work. But it's getting pretty close.

--- FlatCityMaker/Assets/MainScript.cs
+++ FlatCityMaker/Assets/MainScript.cs
@@ -18,17 +18,58 @@ public class MainScript : MonoBehaviour
 
     void Start()
     {
+        IEnumerable<CellVisualBlueprint> allOptions = GetSymmetricalOptions();
         mainGrid = new MainGrid(Width, Height, options);
+        
         CreateDisplayTiles();
+        ResetGridFills();
+    }
+
+    private void ResetGridFills()
+    {
+        foreach (var item in mainGrid.Cells)
+        {
+            item.Reset();
+        }
+    }
+
+    private IEnumerable<CellVisualBlueprint> GetSymmetricalOptions()
+    {
+        List<CellVisualBlueprint> ret = new List<CellVisualBlueprint>(options);
+        foreach (CellVisualBlueprint option in options)
+        {
+            if(option.GetIsAsymmetrical())
+            {
+                ret.Add(option.GetHorizontallyFlipped());
+            }
+        }
+        return ret;
     }
 
     private void Update()
     {
-        if(Input.GetMouseButtonUp(0))
+        HandleInteraction();
+
+        if (mainGrid.EmptyCells.Any())
+        {
+            if (mainGrid.DirtyCells.Any())
+            {
+                mainGrid.DirtyCells.First().UpdateOptions();
+            }
+            else
+            {
+                mainGrid.FillLowestEntropy();
+            }
+        }
+    }
+
+    private void HandleInteraction()
+    {
+        if (Input.GetMouseButtonUp(0))
         {
             HandleClick(true);
         }
-        if(Input.GetMouseButtonUp(1))
+        if (Input.GetMouseButtonUp(1))
         {
             HandleClick(false);
         }
@@ -49,6 +90,7 @@ public class MainScript : MonoBehaviour
             {
                 cell.CycleDesignation(-1);
             }
+            ResetGridFills();
         }
     }
 
@@ -81,39 +123,55 @@ public class MainScript : MonoBehaviour
 [Serializable]
 public class CellVisualBlueprint
 {
-    public Texture2D Texture => texture;
-    public CellDesignation CoreDesignatin => coreDesignation;
-    public CellDesignation LeftConnection => leftConnection;
-    public CellDesignation RightConnection => rightConnection;
-    public CellDesignation UpConnection => upConnection;
-    public CellDesignation DownConnection => downConnection;
-    [SerializeField]
-    private CellDesignation coreDesignation = CellDesignation.Empty;
-    [SerializeField]
-    private CellDesignation leftConnection = CellDesignation.Empty;
-    [SerializeField]
-    private CellDesignation upConnection = CellDesignation.Empty;
-    [SerializeField]
-    private CellDesignation rightConnection = CellDesignation.Empty;
-    [SerializeField]
-    private CellDesignation downConnection = CellDesignation.Empty;
-    [SerializeField]
-    private Texture2D texture = null;
+    public Texture2D Texture;
+    public CoreDesignation Core;
+    public ConnectionType Left;
+    public ConnectionType Right;
+    public ConnectionType Up;
+    public ConnectionType Down;
+    public ConnectionType UpLeft;
+    public ConnectionType UpRight;
+    public ConnectionType DownLeft;
+    public ConnectionType DownRight;
+
+    public bool GetIsAsymmetrical()
+    {
+        return Left != Right
+            || UpLeft != UpRight
+            || DownLeft != DownRight;
+    }
+
+    public CellVisualBlueprint GetHorizontallyFlipped()
+    {
+        CellVisualBlueprint ret = new CellVisualBlueprint();
+        ret.Texture = Texture;
+        ret.Core = Core;
+        ret.Up = Up;
+        ret.Down = Down;
+
+        ret.Left = Right;
+        ret.Right = Left;
+        ret.UpLeft = UpRight;
+        ret.UpRight = UpLeft;
+        ret.DownLeft = DownRight;
+        ret.DownRight = DownLeft;
+        return ret;
+    }
 }
 
 public class GridCellBehavior : MonoBehaviour
 {
     private int designationCycleIndex;
-    private static Dictionary<CellDesignation, Color> debugColors = new Dictionary<CellDesignation, Color>()
+    private static Dictionary<CoreDesignation, Color> debugColors = new Dictionary<CoreDesignation, Color>()
     {
-        {CellDesignation.Empty, Color.white },
-        {CellDesignation.Filled, Color.grey },
+        {CoreDesignation.Empty, Color.white },
+        {CoreDesignation.Filled, Color.grey },
     };
 
-    private static CellDesignation[] DesignationCycle = new CellDesignation[] { CellDesignation.Empty, CellDesignation.Filled };
+    private static CoreDesignation[] DesignationCycle = new CoreDesignation[] { CoreDesignation.Empty, CoreDesignation.Filled };
     private Material mat;
 
-    public IGridCell Model { get; set; }
+    public GridCell Model { get; set; }
 
     internal void CycleDesignation(int by)
     {
@@ -130,8 +188,12 @@ public class GridCellBehavior : MonoBehaviour
     {
         if(Model != null)
         {
-            Color designationColor = debugColors[Model.Designation];
-            mat.SetColor("_Color", designationColor);
+            //Color designationColor = debugColors[Model.Designation];
+            //mat.SetColor("_Color", designationColor);
+            if(Model.FilledWith != null)
+            {
+                mat.SetTexture("_MainTex", Model.FilledWith.Texture);
+            }
         }
     }
 }
@@ -143,7 +205,10 @@ public class MainGrid
 
     public IEnumerable<CellVisualBlueprint> AllOptions { get; }
 
-    public IGridCell[,] Cells { get; private set; }
+    public HashSet<GridCell> EmptyCells { get; } = new HashSet<GridCell>();
+    public HashSet<GridCell> DirtyCells { get; } = new HashSet<GridCell>();
+
+    public GridCell[,] Cells { get; private set; }
 
     public MainGrid(int width, int height, IEnumerable<CellVisualBlueprint> allOptions)
     {
@@ -151,67 +216,63 @@ public class MainGrid
         this.width = width;
         this.height = height;
         Cells = CreateCells();
+        EmptyCells = GetAllCells();
+        DirtyCells = GetAllCells();
     }
 
-    private IGridCell[,] CreateCells()
+    private HashSet<GridCell> GetAllCells()
     {
-        IGridCell[,] ret = new IGridCell[width, height];
+        HashSet<GridCell> ret = new HashSet<GridCell>();
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                ret[x, y] = new GridCell(x, y);
+                ret.Add(Cells[x, y]);
+            }
+        }
+        return ret;
+    }
+
+    public void FillLowestEntropy()
+    {
+        GridCell cellWithLeastOptions = EmptyCells.OrderBy(item => item.Options.Count).First();
+        cellWithLeastOptions.FillSelfWithRandomOption();
+    }
+
+    private GridCell[,] CreateCells()
+    {
+        GridCell[,] ret = new GridCell[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                ret[x, y] = new GridCell(this, x, y);
             }
         }
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                ret[x, y].SetNeighbors(ret);
+                ret[x, y].EstablishNeighbors(ret);
             }
         }
         return ret;
     }
 }
 
-public interface IGridCell
-{
-    CellDesignation Designation { get; set; }
-
-    void SetNeighbors(IGridCell[,] grid);
-}
-
-public class OffGridCell : IGridCell
-{
-    public static OffGridCell GroundCell = new OffGridCell(CellDesignation.Ground);
-    public static OffGridCell SkyCell = new OffGridCell(CellDesignation.Empty);
-
-    public CellDesignation Designation { get; set; }
-
-    public OffGridCell(CellDesignation designation)
-    {
-        Designation = designation;
-    }
-
-    public void SetNeighbors(IGridCell[,] grid)
-    { }
-}
-
-public class GridCell : IGridCell
+public class GridCell
 {
     private readonly MainGrid main;
 
     public int X { get; }
     public int Y { get; }
 
-    public IGridCell LeftNeighbor { get; private set; }
-    public IGridCell RightNeighbor { get; private set; }
-    public IGridCell UpNeighbor { get; private set; }
-    public IGridCell DownNeighbor { get; private set; }
+    public CoreDesignation Designation { get; set; }
 
-    public CellDesignation Designation { get; set; }
+    public IReadOnlyList<CellVisualBlueprint> Options { get; private set; }
+    public CellVisualBlueprint FilledWith { get; private set; }
 
-    private IEnumerable<CellVisualBlueprint> options;
+    private IReadOnlyCollection<Neighbor> neighbors;
 
     public GridCell(MainGrid main, int x, int y)
     {
@@ -220,36 +281,101 @@ public class GridCell : IGridCell
         Y = y;
     }
 
-    public void SetNeighbors(IGridCell[,] cells)
+    public void EstablishNeighbors(GridCell[,] cells)
     {
-        int width = cells.GetLength(0);
-        int height = cells.GetLength(1);
-        LeftNeighbor = X > 0 ? cells[X - 1, Y] : OffGridCell.SkyCell;
-        RightNeighbor = X < width - 1 ? cells[X + 1, Y] : OffGridCell.SkyCell;
-        DownNeighbor = Y > 0 ? cells[X, Y - 1] : OffGridCell.GroundCell;
-        UpNeighbor = Y < height - 1 ? cells[X, Y + 1] : OffGridCell.SkyCell;
+        neighbors = new Neighbor[]
+        {
+            new Neighbor(this, cells, -1, 0, item => item.Left, item => item.Right),
+            new Neighbor(this, cells, 1, 0, item => item.Right, item => item.Left),
+            new Neighbor(this, cells, 0, 1, item => item.Up, item => item.Down),
+            new Neighbor(this, cells, 0, -1, item => item.Down, item => item.Up),
+            new Neighbor(this, cells, -1, 1, item => item.UpLeft, item => item.DownRight),
+            new Neighbor(this, cells, 1, 1, item => item.UpRight, item => item.DownLeft),
+            new Neighbor(this, cells, -1, -1, item => item.DownLeft, item => item.UpRight),
+            new Neighbor(this, cells, 1, -1, item => item.DownRight, item => item.UpLeft)
+        }.Where(item => item.Cell != null).ToList();
+    }
+
+    public void Reset()
+    {
+        FilledWith = null;
+        Options = main.AllOptions.Where(item => item.Core == Designation).ToArray();
+        main.DirtyCells.Add(this);
+        main.EmptyCells.Add(this);
     }
 
     public void UpdateOptions()
     {
-        options = main.AllOptions.Where(OptionIsValid).ToArray();
+        Options = main.AllOptions.Where(OptionIsValid).ToArray();
+        main.DirtyCells.Remove(this);
     }
 
     private bool OptionIsValid(CellVisualBlueprint blueprint)
     {
-        return Designation == blueprint.CoreDesignatin
-            && UpNeighbor.Designation == blueprint.UpConnection
-            && DownNeighbor.Designation == blueprint.DownConnection
-            && LeftNeighbor.Designation == blueprint.LeftConnection
-            && RightNeighbor.Designation == blueprint.RightConnection;
+        return Designation == blueprint.Core
+            && neighbors.All(item => item.DoesConnectTo(blueprint));
+    }
+
+    internal void FillSelfWithRandomOption()
+    {
+        CellVisualBlueprint[] optionsArray = Options.ToArray();
+        int rand = UnityEngine.Random.Range(0, optionsArray.Length);
+        FilledWith = optionsArray[rand];
+        main.EmptyCells.Remove(this);
+        foreach (Neighbor neighbor in neighbors.Where(item => item.Cell.FilledWith == null))
+        {
+            main.DirtyCells.Add(neighbor.Cell);
+        }
+    }
+
+    private class Neighbor
+    {
+        public GridCell Cell { get; }
+        private readonly Func<CellVisualBlueprint, ConnectionType> selfSelector;
+        private readonly Func<CellVisualBlueprint, ConnectionType> neighborSelector;
+
+        public Neighbor(GridCell source,
+            GridCell[,] cells, 
+            int xOffset, 
+            int yOffset,
+            Func<CellVisualBlueprint, ConnectionType> selfSelector,
+            Func<CellVisualBlueprint, ConnectionType> neighborSelector)
+        {
+            int xIndex = source.X + xOffset;
+            int yIndex = source.Y + yOffset;
+
+            this.selfSelector = selfSelector;
+            this.neighborSelector = neighborSelector;
+
+            int width = cells.GetLength(0);
+            int height = cells.GetLength(1);
+
+            bool spaceLeft = xIndex > 0;
+            bool spaceRight = xIndex < width - 1;
+            bool spaceDown = yIndex > 0;
+            bool spaceUp = yIndex < height - 1;
+            if(spaceLeft && spaceRight && spaceDown && spaceUp)
+            {
+                Cell = cells[xIndex, yIndex];
+            }
+        }
+
+        public bool DoesConnectTo(CellVisualBlueprint blueprint)
+        {
+            return Cell.Options.Any(item => selfSelector(item) == neighborSelector(blueprint));
+        }
     }
 }
 
-public enum CellDesignation
+public enum CoreDesignation
 {
     Empty,
     Filled,
-    Ground,
-    SupportPillar
+}
+public enum ConnectionType
+{
+    Empty,
+    Filled,
+    Struct
 }
 
