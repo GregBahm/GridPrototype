6a1e64d01574ffbc0bd3fbe56264f53ec9cf6ddf
Greg Bahm <grbahm@microsoft.com>
2020-11-07 04:18:35 -0800

    Might be close having the grid solver figured out.

--- FlatCityMaker/Assets/Scripts/GridSolver.cs
+++ FlatCityMaker/Assets/Scripts/GridSolver.cs
@@ -1,104 +1,138 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using TileDefinition;
 
-public class GridSolver
+namespace GridSolver
 {
-    private readonly MainGrid currentGrid;
-    private ResolvedCell resolvedStartCell;
-
-    public GridSolver(MainGrid currentGrid, GridCell solveStartCell)
+    public class Solver
     {
-        this.currentGrid = currentGrid;
-        this.resolvedStartCell = GetStartCellResolved(solveStartCell);
-    }
+        public SolverCell[,] cells;
 
-    public void ApplySolve()
-    {
-        RecursivelyApplyToGrid(resolvedStartCell);
-    }
-
-    private void RecursivelyApplyToGrid(ResolvedCell cell)
-    {
-        cell.GridCell.FilledWith = cell.Choice;
-        foreach (ResolvedCell child in cell.ResolvedNeighbors)
+        public void Solve()
         {
-            RecursivelyApplyToGrid(child);
+            GridState state = CreateInitialState();
+            GridState solved = RecursivelySolve(state);
         }
-    }
 
-    private ResolvedCell GetStartCellResolved(GridCell solveStartCell)
-    {
-        throw new NotImplementedException();
-    }
-}
-
-
-public class ResolvingCell
-{
-    private IReadOnlyList<Tile> options;
-    
-    private IReadOnlyCollection<ResolvedCell> resolvedNeighbors;
-    private IReadOnlyCollection<UnresolvedCell> unresolvedNeighbors;
-
-    public bool ChoiceIsValid { get; }
-    public bool HasUnsolvedNeighbors { get; }
-
-    public bool Resolveable { get { return Resolved != null; } }
-    public ResolvingCell Resolved { get; }
-
-    public ResolvingCell(List<Tile> options,
-        List<ResolvedCell> resolvedNeighbors,
-        List<UnresolvedCell> unresolvedNeighbors)
-    {
-        this.options = options;
-        this.resolvedNeighbors = resolvedNeighbors;
-        this.unresolvedNeighbors = unresolvedNeighbors;
-
-        ChoiceIsValid = resolvedNeighbors.All(neighbor => neighbor.ConnectionIsValid(Choice));
-
-        HasUnsolvedNeighbors = unresolvedNeighbors.Any();
-
-        Resolved = TryGetResolved();
-    }
-
-    public ResolvingCell TryGetResolved()
-    {
-        foreach (Tile tile in options)
+        private GridState CreateInitialState()
         {
-            ResolvingCell resolved = TryGetResolved(tile);
-            if (resolved != null)
+            throw new NotImplementedException();
+        }
+
+        private GridState RecursivelySolve(GridState gridState)
+        {
+            GridCellState unsolvedCell = gridState.UnsolvedStates.First();
+            foreach (Tile choice in unsolvedCell.AvailableOptions)
             {
-                return resolved;
+                GridState state = gridState.GetWithChoiceApplied(choice);
+                if(state != null) // A grid cell was left with no valid options
+                {
+                    if (!state.UnsolvedStates.Any())
+                    {
+                        return gridState;
+                    }
+                    return RecursivelySolve(state);
+                }
             }
+            throw new Exception("This grid appears to be unsolveable");
         }
-        return null;
     }
 
-    private ResolvingCell TryGetResolved(Tile choice)
+    public class SolverCell
     {
-        List<ResolvedCell> resolvedNeighbors = new List<ResolvedCell>();
-        foreach (UnresolvedCell unresolvedCell in unresolvedNeighbors)
+        public IEnumerable<Tile> BaseOptions { get; }
+
+        public IEnumerable<SolverCellNeighbor> Neighbors { get; }
+        
+        public class SolverCellNeighbor
+        { }
+    }
+
+    public class GridState
+    {
+        public GridCellState[,] grid;
+        public IReadOnlyCollection<GridCellState> UnsolvedStates { get; }
+
+        public GridState(GridCellState[,] grid, IReadOnlyCollection<GridCellState> unsolvedStates)
         {
-            ResolvedCell resolved =
+            this.grid = grid;
+            UnsolvedStates = unsolvedStates;
         }
-        return
+        
+        public GridState GetWithChoiceApplied(Tile choice)
+        {
+            GridCellState[,] newGrid = (GridCellState[,])grid.Clone();
+            GridCellState first = UnsolvedStates.First();
+            GridCellState newState = new GridCellState(first.X, first.Y, choice);
+            newGrid[newState.X, newState.Y] = newState;
+
+            List<GridCellState> newUnsolved = new List<GridCellState>();
+            foreach (GridCellState unsolvedCell in UnsolvedStates.Skip(1))
+            {
+                // TODO: Optimize to only update neighbors
+                GridCellState updatedCell = GetUpdatedCell(unsolvedCell, newGrid);
+                if(updatedCell.Status == GridCellState.StateStatus.Unsolveable)
+                {
+                    return null;
+                }
+
+                newGrid[updatedCell.X, updatedCell.Y] = updatedCell;
+
+                if(updatedCell.Status == GridCellState.StateStatus.Unsolved)
+                {
+                    newUnsolved.Add(updatedCell);
+                }
+            }
+            return new GridState(newGrid, newUnsolved);
+        }
+
+        private GridCellState GetUpdatedCell(GridCellState unsolvedCell, GridCellState[,] newGrid)
+        {
+            List<Tile> newValidOptions = unsolvedCell.AvailableOptions.Where(option => OptionIsValid(unsolvedCell.X, unsolvedCell.Y, option, newGrid)).ToList();
+            if(newValidOptions.Count == 1)
+            {
+                return new GridCellState(unsolvedCell.X, unsolvedCell.Y, newValidOptions[0]);
+            }
+            return new GridCellState(unsolvedCell.X, unsolvedCell.Y, newValidOptions);
+        }
+
+        private bool OptionIsValid(int x, int y, Tile option, GridCellState[,] newGrid)
+        {
+            throw new NotImplementedException();
+        }
     }
-}
 
-public class UnresolvedCell
-{
-}
-
-public class ResolvedCell
-{
-    public GridCell GridCell { get; }
-    public Tile Choice { get; }
-    public IEnumerable<ResolvedCell> ResolvedNeighbors { get; }
-    
-    public bool ConnectionIsValid(Tile tile)
+    public class GridCellState
     {
-        throw new NotImplementedException();
+        public int X { get; }
+        public int Y { get; }
+        public Tile Choice { get; }
+        public IEnumerable<Tile> AvailableOptions { get; }
+        public StateStatus Status { get; }
+
+        public GridCellState(int x, int y, IEnumerable<Tile> options)
+        {
+            X = x;
+            Y = y;
+            AvailableOptions = options;
+            Status = AvailableOptions.Any() ? StateStatus.Unsolved : StateStatus.Unsolveable;
+        }
+
+        public GridCellState(int x, int y, Tile choice)
+        {
+            X = x;
+            Y = y;
+            Choice = choice;
+            Status = StateStatus.Solved;
+        }
+
+        public enum StateStatus
+        {
+            Solved,
+            Unsolved,
+            Unsolveable
+        }
     }
 }
